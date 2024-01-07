1b09a949bacb3edf2c8466395f63b0c409c1e17d
unknown <sajinkhan300@gmail.com>
2024-01-07 16:44:04 +0600

    new line added

--- C(Practice Sessions)/Git Learing session/session1.c
+++ C(Practice Sessions)/Git Learing session/session1.c
@@ -4,5 +4,6 @@ int main()
     printf("Recently I heard that you've achieved 95%% marks in your exam.\n");
     printf("This is brilliant!\n");
     printf("I wish you'll shine in your life!  Good Luck with all the barriers(^) in your life.");
+    printf("new line added");
     return 0;
 }
